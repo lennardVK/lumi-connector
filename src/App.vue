2ef29feb70cb4a266f1f8dc42635564733b04cd6
Lennard Kollossa <l.kollossa@web.de>
2020-04-16 11:22:28 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,82 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <div>To do: {{ toDo }}</div>
+    <div>Doing: {{ doing }}</div>
+    <div>Done: {{ done }}</div>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: Object,
+      lists: [],
+      toDo: 0,
+      doing: 0,
+      done: 0
+    };
+  },
+  created() {
+    setInterval(() => {
+      this.getData();
+    }, 1000);
+  },
+  methods: {
+    convertData() {
+      if (this.data != undefined) {
+        let cards = this.data.map(card => {
+          let listId = card.idList;
+          return listId;
+        });
+        this.list = cards;
+      }
+    },
+
+    setStates() {
+      let toDo = this.list.filter(id => id == "5e93f1619d3ddd839efd7857")
+        .length;
+      let doing = this.list.filter(id => id == "5e93f1643458932cd6bae158")
+        .length;
+      let done = this.list.filter(id => id == "5e93f1679030046a66c83cb1")
+        .length;
+
+      this.toDo = toDo;
+      this.doing = doing;
+      this.done = done;
+
+      /* console.log(
+        "To do: ",
+        this.toDo,
+        "doing: ",
+        this.doing,
+        "done: ",
+        this.done
+      );*/
+    },
+
+    getData() {
+      const fetch = require("node-fetch"); // key=e0bb68c2d5670dfd4e12f6b8717522eb&token=3d4f06e55cba12ccaeb5d5698c4613858ba7783c540f0e1d084b3e348063749a
+      fetch("https://api.trello.com/1/boards/5e93f15200669f33e504f331/cards", {
+        method: "GET"
+      })
+        .then(response => {
+          console.log(`Response: ${response.status} ${response.statusText}`);
+
+          return response.text(response);
+        })
+        .then(data => {
+          this.data = JSON.parse(data);
+          console.log(this.data);
+          this.convertData();
+          this.setStates();
+        })
+        .catch(err => console.error(err));
+    }
+  }
+};
+</script>
 
 <style>
 #app {
